60979d9c4ed4e48d62e6f2856a803f71e9f1c254
Feel <philipp.bhandari@gmail.com>
2019-04-25 10:18:14 +0300

    drum done

--- html-element-collection/drum-machine/js/script.js
+++ html-element-collection/drum-machine/js/script.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const drumButtons = document.getElementsByClassName('drum-kit__drum');
+for (let button of drumButtons) {
+    button.addEventListener('click', ()=>{
+        button.getElementsByTagName('audio')[0].play();
+    });
+}
