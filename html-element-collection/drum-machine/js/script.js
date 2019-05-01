5e6daf2136b7de0fdce31c0592f43b7d33d02557
Feel <philipp.bhandari@gmail.com>
2019-05-01 18:13:36 +0300

    fix drums

--- html-element-collection/drum-machine/js/script.js
+++ html-element-collection/drum-machine/js/script.js
@@ -3,6 +3,9 @@
 const drumButtons = document.getElementsByClassName('drum-kit__drum');
 for (let button of drumButtons) {
     button.addEventListener('click', ()=>{
-        button.getElementsByTagName('audio')[0].play();
+        let audio = button.getElementsByTagName('audio')[0];
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
     });
 }
