a29491cae60fd1333ea1773bf8de22c5744cd943
Feel <philipp.bhandari@gmail.com>
2019-05-08 09:02:05 +0300

    weather is fine

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,9 @@
-const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
-request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+function  onLoad() {
+    const response = JSON.parse(this.responseText);
+    setData(response);
 }
+
+const request = new XMLHttpRequest();
+request.addEventListener('load', onLoad);
+request.open('GET', 'https://neto-api.herokuapp.com/weather');
+request.send();
