ee4ff2edda73f89f1700e42859e5360f84833bc7
Feel <philipp.bhandari@gmail.com>
2019-05-16 13:55:56 +0300

    table done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    let target = event.target,
+        classList = target.classList,
+        dataset = target.dataset;
+
+    if(classList.contains('prop__name')) {
+        event.currentTarget.dataset.sortBy = dataset.propName;
+        switch (dataset.dir) {
+            case '1':
+                dataset.dir = '-1';
+                break;
+            case '-1':
+                dataset.dir = '1';
+                break;
+            default:
+                dataset.dir = '1';
+        }
+
+        sortTable(dataset.propName, dataset.dir);
+    }
 }
