c2f39abcc2bd0c9b5f1a5e4ce1c183330ddf8a53
Feel <philipp.bhandari@gmail.com>
2019-05-16 14:20:16 +0300

    shop done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const listItems = document.querySelector('.items-list');
+function addItem(e) {
+    if(e.target.classList.contains('add-to-cart')) {
+        const obj = {
+            title: e.target.dataset.title,
+            price: e.target.dataset.price
+        };
+
+        addToCart(obj);
+    }
+}
+listItems.addEventListener('click', addItem);
