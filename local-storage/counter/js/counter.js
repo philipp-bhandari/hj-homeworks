7431c6bae266256dd5180f87ff563d394ba33ef2
Feel <philipp.bhandari@gmail.com>
2019-05-17 13:24:00 +0300

    counter done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const counterblock = document.getElementById('counter'),
+    incrementButton = document.getElementById('increment'),
+    decrementButton = document.getElementById('decrement'),
+    resetButton = document.getElementById('reset');
+
+
+function changeCounter(e) {
+    localStorage.counter
+        = counterblock.textContent
+        = save(e.target.id);
+}
+
+function save(operation){
+    switch (operation) {
+        case 'increment':
+            return parseInt(counterblock.textContent) + 1;
+        case 'decrement':
+            return counterblock.textContent !== '0' ? parseInt(counterblock.textContent) - 1 : 0;
+        case 'reset':
+            return 0;
+    }
+}
+counterblock.textContent = localStorage.counter || 0;
+
+incrementButton.addEventListener('click', changeCounter);
+decrementButton.addEventListener('click', changeCounter);
+resetButton.addEventListener('click', changeCounter);
