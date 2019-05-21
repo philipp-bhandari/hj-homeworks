c20015f8cc57376258e5fb55a7983741bb6df379
Feel <philipp.bhandari@gmail.com>
2019-05-21 09:05:23 +0300

    signin done

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const signUpForm = document.querySelector('.sign-up-htm'),
+    signInForm = document.querySelector('.sign-in-htm'),
+    errorUp = signUpForm.querySelector('.error-message'),
+    errorIn = signInForm.querySelector('.error-message');
+
+function submitForm(e) {
+    e.preventDefault();
+    let formData = new FormData(this);
+
+    const sendObj = {};
+    for (const [k, v] of formData) {
+        sendObj[k] = v;
+    }
+
+    let link = this === signUpForm ? 'signup' : 'signin';
+
+    const promise = fetch(`https://neto-api.herokuapp.com/${link}`, {
+        method: 'POST',
+        body: JSON.stringify(sendObj),
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    promise.then((res) => {
+        if (200 <= res.status && res.status < 300) {
+            return res;
+        }
+        throw new Error(res.statusText);
+    }).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        if(data.error) {
+            if(link === 'signup') {
+                errorUp.textContent = data.message;
+            } else {
+                errorIn.textContent = data.message;
+            }
+        } else {
+            if(link === 'signup') {
+                errorUp.textContent = `Пользователь ${data.name} успешно авторизован`;
+            } else {
+                errorIn.textContent = `Пользователь ${data.name} успешно зарегистрирован`;
+            }
+        }
+    }).catch((err) => {
+        console.log('network error');
+    });
+}
+
+signUpForm.addEventListener('submit', submitForm);
+signInForm.addEventListener('submit', submitForm);
