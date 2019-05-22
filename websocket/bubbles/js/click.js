34258d7d2312ae9d5f9ec9a5e46ddbfe026e31c9
Feel <philipp.bhandari@gmail.com>
2019-05-22 17:07:38 +0300

    bubbles done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const socket = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+showBubbles(socket);
+
+socket.addEventListener('open', ()=>{
+    console.log('Соединение открыто');
+});
+
+document.addEventListener('click', (e)=>{
+    let obj = {
+        x: e.pageX,
+        y: e.pageY
+    };
+
+    obj = JSON.stringify(obj);
+    socket.send(obj);
+});
