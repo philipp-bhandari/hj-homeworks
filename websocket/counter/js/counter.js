b0d4ae8a40234a3442e62e5d4a52bc63948fd4c6
Feel <philipp.bhandari@gmail.com>
2019-05-22 17:33:33 +0300

    counter done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const counterSpan = document.querySelector('.counter'),
+    errorsOutput = document.querySelector('.errors'),
+    socket = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+socket.addEventListener('open', ()=>{
+    console.log('Соединение открыто');
+});
+
+socket.addEventListener('message', event => {
+    const data = JSON.parse(event.data);
+    counterSpan.innerHTML = data.connections;
+    errorsOutput.innerHTML = data.errors;
+});
+
+window.addEventListener('beforeunload',()=>{
+    socket.onclose = () => {
+        console.log('Соединение закрыто');
+    };
+    socket.close(1000);
+});
