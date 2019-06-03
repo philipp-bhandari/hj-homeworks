3ba974abfa538476423be4e610af0384188c0c4a
Feel <philipp.bhandari@gmail.com>
2019-06-03 08:03:37 +0300

    canvas done

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,31 +1,63 @@
 'use strict';
 
-function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+
+function commentWorker(list) {
+
+    // Обрабатываем список объектов коментариев
+    // Для каждого вызываем функцию addComm, результат добавляем в Div .comments
+
+    const commentsSelector = document.querySelector('.comments');
+    const newComm = list.map(addComm);
+    newComm.forEach(item => commentsSelector.appendChild(item));
 }
 
-function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+function tagElement(tagName, attributes, children) {
+    const element = document.createElement(tagName);
+
+    // Создаем элементы, присваиваем атрибуты, добавляем потомков родителю
+
+    if (typeof attributes === 'object') {
+        Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+    }
+
+    if (typeof children === 'string') {
+        element.textContent = children;
+    } else if (children instanceof Array) {
+        children.forEach(child => element.appendChild(child));
+    }
+
+    return element;
+}
+
+function addComm(comment) {
+    const textList = comment.text.split('\n');
+
+    // Получаем в массив текста из сообщений
+    // Создаем структуру комментариев
+
+    return tagElement('div', {class: 'comment-wrap'}, [
+        tagElement('div', {class: 'photo', title: comment.author.name}, [
+            tagElement('div', {class: 'avatar', style: `background-image: url('${comment.author.pic}')`})
+        ]),
+        tagElement('div', {class: 'comment-block'}, [
+            tagElement('p', {class: 'comment-text'}, textList.map(item => {
+                const br = tagElement('br');
+                const span = tagElement('span');
+                span.textContent = item;
+                span.appendChild(br);
+                return span;
+            })),
+            tagElement('div', {class: 'bottom-comment'}, [
+                tagElement('div', {class: 'comment-date'}, new Date(comment.date).toLocaleString('ru-Ru')),
+                tagElement('ul', {class: 'comment-actions'}, [
+                    tagElement('li', {class: 'complain'}, 'Пожаловаться'),
+                    tagElement('li', {class: 'reply'}, 'Ответить')
+                ])
+            ])
+        ])
+    ]);
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+    .then(res => res.json())
+    .then(commentWorker);
--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -1,1 +1,190 @@
-'use strict'
+'use strict';
+
+const selectId = document.getElementById('acSelect'); // Выбор id самолёта
+const btnSeatMap = document.getElementById('btnSeatMap'); // Кнопка отображения схемы
+const btnSetFull = document.getElementById('btnSetFull'); // Кнопка обозначения всех мест в самолете занятыми
+const btnSetEmpty = document.getElementById('btnSetEmpty'); // Кнопка обозначения всех мест в самолете свободными
+const seatMapTitle = document.getElementById('seatMapTitle'); // Информация о выбранном самолете и количестве пассажиров
+const seatMapDiv = document.getElementById('seatMapDiv'); // Схема мест в самолете
+const totalPax = document.getElementById('totalPax'); // Общее количество занятых мест
+const totalAdult = document.getElementById('totalAdult'); // Общее количество мест с полной стоимостью
+const totalHalf = document.getElementById('totalHalf'); // Общее количество детских мест
+
+btnSetFull.disabled = true;
+btnSetEmpty.disabled = true;
+
+function takeData(value) {
+
+    //Получаем данные, вставляем title
+
+    fetch(`https://neto-api.herokuapp.com/plane/${value}`)
+        .then(response => {
+            if (200 <= response.status && response.status < 300) {
+                return response;
+            }
+            throw new Error(response.statusText);
+        })
+        .then(result => result.json())
+        .then(data => {
+            seatMapTitle.textContent = `${data.title} (${data.passengers} пассажиров)`;
+            btnShowMap(data);
+        });
+}
+
+function btnShowMap(data) {
+
+    // При клике обнуляем количество мест
+
+    btnSeatMap.addEventListener('click', (e) => {
+        e.preventDefault();
+        totalPax.textContent = totalAdult.textContent = totalHalf.textContent = 0;
+        generateMap(data);
+    });
+}
+
+function generateMap(data) {
+    btnSetFull.disabled = false;
+    btnSetEmpty.disabled = false;
+
+    while (seatMapDiv.firstChild) {
+        seatMapDiv.removeChild(seatMapDiv.firstChild);
+    }
+
+    // Обновляем блок схемы мест, создаем новые элементы
+
+    data.scheme.forEach((item, i) => {
+        const letters = item === 4 ? [].concat('', data.letters4, '') : ((item === 6) ? data.letters6 : []);
+        const row = newEl('div', {class: 'row seating-row text-center'}, [
+            newEl('div', {class: 'col-xs-1 row-number'}, [
+                newEl('h2', null, `${i + 1}`)
+            ])
+        ]);
+        const colLeft = newEl('div', {class: 'col-xs-5'});
+        const colRight = newEl('div', {class: 'col-xs-5'});
+
+        letters.forEach((letter, i) => {
+            if (i <= 2) {
+                colLeft.appendChild(generateSeat(letter));
+            } else {
+                colRight.appendChild(generateSeat(letter));
+            }
+        });
+
+        row.appendChild(colLeft);
+        row.appendChild(colRight);
+
+        seatMapDiv.appendChild(row);
+    });
+}
+
+function generateSeat(value) {
+    if (value !== '') {
+        const seat = newEl('div', {class: 'col-xs-4 seat'}, [
+            newEl('span', {class: 'seat-label'}, value)
+        ]);
+
+        seat.addEventListener('click', pickOneSeat);
+
+        return seat;
+    } else {
+        return newEl('div', {class: 'col-xs-4 no-seat'});
+    }
+}
+
+function cntOfSeat() {
+    const seats = seatMapDiv.querySelectorAll('.seat');
+
+    Array.from(seats).forEach(item => {
+        if (item.classList.contains('adult') || item.classList.contains('half')) {
+            totalPax.textContent = (parseInt(totalPax.textContent) < seats.length) ? parseInt(totalPax.textContent) + 1 : seats.length;
+        } else {
+            totalPax.textContent = (parseInt(totalPax.textContent) > 0) ? parseInt(totalPax.textContent) - 1 : 0;
+        }
+
+        if (item.classList.contains('adult')) {
+            totalAdult.textContent = (parseInt(totalAdult.textContent) < seats.length) ? parseInt(totalAdult.textContent) + 1 : seats.length;
+        } else {
+            totalAdult.textContent = (parseInt(totalAdult.textContent) > 0) ? parseInt(totalAdult.textContent) - 1 : 0;
+        }
+
+        if (item.classList.contains('half')) {
+            totalHalf.textContent = (parseInt(totalHalf.textContent) < seats.length) ? parseInt(totalHalf.textContent) + 1 : seats.length;
+        } else {
+            totalHalf.textContent = (parseInt(totalHalf.textContent) > 0) ? parseInt(totalHalf.textContent) - 1 : 0;
+        }
+    });
+}
+
+function checkSeat(type) {
+    const seatList = seatMapDiv.querySelectorAll('.seat');
+
+    // Добавляем или удаляем классы
+
+    Array.from(seatList).forEach(item => {
+        if (!item.classList.contains('adult') && type === 'add') {
+            item.classList.add('adult');
+        } else if ((item.classList.contains('adult') || item.classList.contains('half')) && type === 'remove') {
+            item.classList.remove('adult', 'half');
+        }
+    });
+
+    cntOfSeat();
+}
+
+function pickOneSeat(event) {
+    const target = event.currentTarget;
+
+    // Добавляем выбор сидения
+
+    if (!target.classList.contains('adult')) {
+        target.classList.add('adult');
+        totalPax.textContent = parseInt(totalPax.textContent) + 1;
+        totalAdult.textContent = parseInt(totalAdult.textContent) + 1;
+    } else {
+        target.classList.remove('adult');
+        totalPax.textContent = parseInt(totalPax.textContent) - 1;
+        totalAdult.textContent = parseInt(totalAdult.textContent) - 1;
+    }
+
+    if (!target.classList.contains('half') && event.altKey) {
+        target.classList.add('half');
+        totalPax.textContent = parseInt(totalPax.textContent) + 1;
+        totalHalf.textContent = parseInt(totalHalf.textContent) + 1;
+    } else if (target.classList.contains('half') && event.altKey) {
+        target.classList.remove('half');
+        totalPax.textContent = parseInt(totalPax.textContent) - 1;
+        totalHalf.textContent = parseInt(totalHalf.textContent) - 1;
+    }
+}
+
+function newEl(tagName, attrs, childs) {
+    const el = document.createElement(tagName);
+
+    // Функция создания тега с атрибутами и потомками
+
+    if (typeof attrs === 'object' && attrs !== null) {
+        Object.keys(attrs).forEach(attr => el.setAttribute(attr, attrs[attr]));
+    }
+
+    if (typeof childs === 'string') {
+        el.textContent = childs;
+    } else if (childs instanceof Array) {
+        childs.forEach(child => el.appendChild(child));
+    }
+
+    return el;
+}
+
+selectId.addEventListener('change', event => takeData(event.target.value));
+
+document.addEventListener('DOMContetLoaded', takeData(selectId.value));
+
+btnSetFull.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkSeat('add');
+});
+
+btnSetEmpty.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkSeat('remove');
+});
