71864cf000bd67bf2b56d95897eb0e51af2c1a0a
Feel <philipp.bhandari@gmail.com>
2019-06-03 08:16:25 +0300

    ws done

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,24 @@
 'use strict';
+const socket = new WebSocket('sockets://neto-api.herokuapp.com/draw');
+
+function sendData(event) {
+    const events = document.getElementById(event.id);
+    const ctx = canvas.getContext('2d');
+    event.canvas.toBlob(blob => socket.send(blob));
+}
+
+socket.addEventListener('open', (event) => {
+    editor.addEventListener('update', sendData);
+});
+
+socket.addEventListener('close', (event) => {
+    editor.removeEventListener('update', sendData);
+});
+
+socket.addEventListener('error', (error) => {
+    console.error(error.data);
+});
+
+window.addEventListener('beforeunload', () => {
+    socket.close(1000, 'Сессия закрыта');
+});
