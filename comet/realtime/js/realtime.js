2eefcf40828fd7834907307840e3d79993625367
Feel <philipp.bhandari@gmail.com>
2019-06-03 08:12:42 +0300

    realtime done

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -1,29 +1,28 @@
 const ctx = document.getElementById('chart').getContext('2d');
-const realtime = new Chart(ctx).Bar({
-  labels: [],
-  datasets: [{
-    fillColor: 'rgba(0,60,100,1)',
-    strokeColor: 'black',
-    data: []
-  }]
+const online = new Chart(ctx).Bar({
+    labels: [],
+    datasets: [{
+        fillColor: 'rgba(0,60,100,1)',
+        strokeColor: 'black',
+        data: []
+    }]
 }, {
-  responsive: true,
-  barValueSpacing: 2
+    responsive: true,
+    barValueSpacing: 2
 });
 
-let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
-ws.addEventListener('message', event => {
-  if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
+let isFirst = true;
 
-    isFirst = false;
-  } else {
-    const [label, data] = event.data.split('|');
-    realtime.removeData();
-    realtime.addData([Number(data)], label);
-  }
+ws.addEventListener('message', event => {
+    const dataModify = JSON.parse(event.data);
+    console.log(dataModify)
+
+    if (isFirst) {
+        dataModify.forEach(data => online.addData([Number(data.online)], data.time));
+        isFirst = false;
+    } else {
+        online.removeData();
+        online.addData([Number(dataModify.online)], dataModify.time);
+    }
 });
