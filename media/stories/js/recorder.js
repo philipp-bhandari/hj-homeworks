f9677c8bd608f40fc8a97f398ba143fbe877ac2d
Feel <philipp.bhandari@gmail.com>
2019-06-06 17:23:22 +0300

    stories done

--- media/stories/js/recorder.js
+++ media/stories/js/recorder.js
@@ -1,46 +1,83 @@
 'use strict';
 
 if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {};
+    navigator.mediaDevices = {};
 }
 
 if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = function (constraints) {
-    var getUserMedia = navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia;
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+        var getUserMedia = navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
 
-    if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+        return new Promise((resolve, reject) => {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
     }
-    return new Promise((resolve, reject) => {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
-  }
 }
 
 function createThumbnail(video) {
-  return new Promise((done, fail) => {
-    const preview = document.createElement('video');
-    preview.src = URL.createObjectURL(video);
-    preview.addEventListener('loadeddata', () => preview.currentTime = 2);
-    preview.addEventListener('seeked', () => {
-      const snapshot = document.createElement('canvas');
-      const context = snapshot.getContext('2d');
-      snapshot.width = preview.videoWidth;
-      snapshot.height = preview.videoHeight;
-      context.drawImage(preview, 0, 0);
-      snapshot.toBlob(done);
+    return new Promise((done, fail) => {
+        const preview = document.createElement('video');
+        preview.src = URL.createObjectURL(video);
+        preview.addEventListener('loadeddata', () => preview.currentTime = 2);
+        preview.addEventListener('seeked', () => {
+            const snapshot = document.createElement('canvas');
+            const context = snapshot.getContext('2d');
+            snapshot.width = preview.videoWidth;
+            snapshot.height = preview.videoHeight;
+            context.drawImage(preview, 0, 0);
+            snapshot.toBlob(done);
+        });
     });
-  });
 }
 
 function record(app) {
-  return new Promise((done, fail) => {
     app.mode = 'preparing';
-    setTimeout(() => {
-      fail('Не удалось записать видео');
-    }, app.limit);
-  });
+
+    return new Promise((done, fail) => {
+
+        navigator.mediaDevices
+            .getUserMedia(app.config)
+            .then((stream) => {
+                app.preview.src = URL.createObjectURL(stream);
+                app.mode = 'recording';
+
+                let recorder = new MediaRecorder(stream);
+                let chunks = [];
+
+                recorder.addEventListener('dataavailable', (e) => chunks.push(e.data));
+                recorder.addEventListener('stop', (e) => {
+                    const recorded = new Blob(chunks, {'type': recorder.mimeType});
+                    chunks = null;
+
+
+                    app.preview.srcObject = null;
+                    stream.getTracks().forEach(track => track.stop());
+                    recorder = stream = null;
+
+                    createThumbnail(recorded)
+                        .then(frame => {
+                            app.mode = 'sending';
+                            done({video: recorded, frame: frame});
+                        });
+                });
+
+                recorder.start(1000);
+
+                setTimeout(() => {
+                    recorder.stop();
+                }, app.limit);
+
+            })
+
+            .catch((err) => {
+                fail(`Не удалось записать видео, ошибка: ${err.name}: ${err.message} \n ${err.stack}`);
+            });
+
+    });
 }
